05fc82ed2738899fc5fdf012d91fca080eaef2fc
kristenwang <kristenwang23@gmail.com>
2016-09-21 00:35:07 -0400

    change

--- startup-systems/homepage.js
+++ startup-systems/homepage.js
@@ -1,5 +1,9 @@
+
+ini();
+function ini(){
 document.getElementById("xian").onclick = function() {window.open('https://en.wikipedia.org/wiki/Xi%27an');};
 document.getElementById("newyork").addEventListener("click", newyork);
 function newyork() {
     window.open('https://en.wikipedia.org/wiki/New_York_City');
+}
 }

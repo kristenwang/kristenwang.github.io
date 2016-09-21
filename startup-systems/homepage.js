559fd172e1a8348f21180dba81839ff1ab26cbcb
kristenwang <kristenwang23@gmail.com>
2016-09-21 00:08:29 -0400

    add JavaScript

---
+++ startup-systems/homepage.js
@@ -0,0 +1,5 @@
+document.getElementById("xian").onclick = function() {window.open('https://en.wikipedia.org/wiki/Xi%27an');};
+document.getElementById("newyork").addEventListener("click", newyork);
+function newyork() {
+    window.open('https://en.wikipedia.org/wiki/New_York_City');
+}
